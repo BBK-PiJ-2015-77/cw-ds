eff290e59af9a2a86d5c1e5db59a0f433bb9283b
Thomas McGarry <mcgarry_tom@hotmail.com>
2015-12-21 13:15:12 +0000

    First commit

---
+++ ArrayList.java
@@ -0,0 +1,19 @@
+public class ArrayList implements List {
+
+	/*
+	 *	@See 'List.java' for a description of these methods
+	 */	
+
+	public boolean isEmpty();
+	
+	public int size();
+	
+	public ReturnObject get(int index);
+	
+	public ReturnObject remove(int index);
+	
+	public ReturnObject add(int index, Object item);
+	
+	public ReturnObject add(Object item);
+
+}
