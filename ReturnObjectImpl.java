1fb8276c4aafd91a8f758499edaeda700938935b
Thomas McGarry <mcgarry_tom@hotmail.com>
2015-12-21 12:34:53 +0000

    Methods implemented and tested

--- ReturnObjectImpl.java
+++ ReturnObjectImpl.java
@@ -1,2 +1,42 @@
 public class ReturnObjectImpl implements ReturnObject {
+	
+	private Object obj;
+	private ErrorMessage em;
+	
+	//@ param?
+	
+	public ReturnObjectImpl(Object obj) {
+		this.obj = obj;
+		this.em = ErrorMessage.NO_ERROR;
+	}
+	
+	public ReturnObjectImpl(ErrorMessage em){
+		this.em = em;
+		this.obj = null;
+	}
+	
+	/*
+	 *	@see 'ReturnObject.java' for description of these methods
+	 */
+	
+	@Override
+	public boolean hasError() {
+		return em != ErrorMessage.NO_ERROR;
+	}
+	
+	
+	@Override
+	public ErrorMessage getError() {
+		return em;
+	}
+	
+	@Override
+	public Object getReturnValue() {
+		if (hasError()) {
+			return null;
+		} else {
+			return obj;
+		}
+	}
+
 }
