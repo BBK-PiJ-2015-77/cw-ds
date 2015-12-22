b22401524c7cf82885020c0c90a1b39e81830b85
Thomas McGarry <mcgarry_tom@hotmail.com>
2015-12-22 15:57:27 +0000

    Completed the first add method. Untested

--- LinkedList.java
+++ LinkedList.java
@@ -9,21 +9,42 @@ public class LinkedList implements List {
 	}
 
 	public boolean isEmpty(){
+		return size == 0;
 	}
 	
 	public int size(){
+		return size;
 	}
 	
 	public ReturnObject get(int index){
+		return null;
 	}
 	
 	public ReturnObject remove(int index){
+		return null;
 	}
 	
 	public ReturnObject add(int index, Object item){
+		return null;
 	}
 	
 	public ReturnObject add(Object item){
+		if (item == null) {
+			return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+		} else if (head == null) {
+			head = new Node(item);
+			size++;
+			return new ReturnObjectImpl(item);
+		} else {
+			Node position = head;
+			while (position.getNext() != null) {
+				position = position.getNext();
+			}
+			position.setNext(new Node(item));
+			size++;
+			return new ReturnObjectImpl(item);
+		}
 	}
+
 
 }
