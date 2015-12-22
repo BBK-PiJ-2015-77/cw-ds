1ba656967ff6cc4317161c3702c13dd69ebd0a72
Thomas McGarry <mcgarry_tom@hotmail.com>
2015-12-22 15:28:30 +0000

    Finished defining the node class and added head

--- LinkedList.java
+++ LinkedList.java
@@ -1,4 +1,29 @@
-public class LinkedList implements Lis {
+public class LinkedList implements List {
 
+	private Node head;
+	private int size;
+	
+	public LinkedList() {
+		head = null;
+		size = 0;
+	}
+
+	public boolean isEmpty(){
+	}
+	
+	public int size(){
+	}
+	
+	public ReturnObject get(int index){
+	}
+	
+	public ReturnObject remove(int index){
+	}
+	
+	public ReturnObject add(int index, Object item){
+	}
+	
+	public ReturnObject add(Object item){
+	}
 
 }
