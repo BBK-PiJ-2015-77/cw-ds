ab5b2d6ecc444724a6c1e2cf3178fddcc9dbb102
Thomas McGarry <mcgarry_tom@hotmail.com>
2015-12-23 17:39:01 +0000

    Still untested

--- FunctionalArrayList.java
+++ FunctionalArrayList.java
@@ -9,23 +9,12 @@ public class FunctionalArrayList extends ArrayList implements FunctionalList {
 		}
 	}
 	
-	/**
-     * Returns a list with the elements in this list except the
-     * head. The elements must be in the same order. The original list
-     * must not change or be affected by changes in the new list. 
-     * 
-     * If the list is empty, another empty list is returned. 
-     */
-	
-	
 	@Override
 	public FunctionalList rest(){
-		if (isEmpty()) {
-			//not finished
-			return null;
-		} else {
 		FunctionalList newList = new FunctionalArrayList();
-		//going to have to use the add function
+		if (isEmpty()) {
+			return newList;
+		} else {
 			for (int i = 1; i < numberOfElements; i++) {
 				newList.add(intArray[i]);
 			}
