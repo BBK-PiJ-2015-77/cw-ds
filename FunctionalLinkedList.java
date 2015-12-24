6457cfaf96ed42a38214b982b3baad2b69a08828
Thomas McGarry <mcgarry_tom@hotmail.com>
2015-12-24 13:05:54 +0000

    Cleaned up, added condition for rest list being empty

--- FunctionalLinkedList.java
+++ FunctionalLinkedList.java
@@ -9,20 +9,12 @@ public class FunctionalLinkedList extends LinkedList implements FunctionalList {
 		}
 	}
 	
-	/**
-     * Returns a list with the elements in this list except the
-     * head. The elements must be in the same order. The original list
-     * must not change or be affected by changes in the new list. 
-     * 
-     * If the list is empty, another empty list is returned. 
-     *
-     *	TM: create a new list copying the elements of the previous list.
-     */
-	
 	@Override
 	public FunctionalList rest() {
-		//if isEmpty()
 		FunctionalList newList = new FunctionalArrayList();
+		if (isEmpty()) {
+			return newList;
+		}
 		Node newHead = head.getNext();
 		newList.add(newHead);
 		for (int i = 0; i < size -1; i++) {
