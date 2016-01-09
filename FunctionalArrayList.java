2d1bc2963028707451b9b7de6f55950e953e58a3
Thomas McGarry <mcgarry_tom@hotmail.com>
2016-01-09 18:23:18 +0000

    Changed to objArray, added some comments and tested

--- FunctionalArrayList.java
+++ FunctionalArrayList.java
@@ -1,11 +1,15 @@
 public class FunctionalArrayList extends ArrayList implements FunctionalList {
-
+	
+	/**
+	 *	{@see FunctionalList} for description of the methods below
+	 */
+	
 	@Override
 	public ReturnObject head(){
 		if (numberOfElements == 0) {
 			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
 		} else {
-			return new ReturnObjectImpl(intArray[0]);
+			return new ReturnObjectImpl(objArray[0]);
 		}
 	}
 	
@@ -16,7 +20,7 @@ public class FunctionalArrayList extends ArrayList implements FunctionalList {
 			return newList;
 		} else {
 			for (int i = 1; i < numberOfElements; i++) {
-				newList.add(intArray[i]);
+				newList.add(objArray[i]);
 			}
 			return newList;
 		}
