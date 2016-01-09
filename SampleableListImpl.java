174bf183754f078d216ce4372282e3a857a2394f
BBK-PiJ-2015-77 <mcgarry_tom@hotmail.com>
2016-01-09 19:55:55 +0000

    Create SampleableListImpl.java

---
+++ SampleableListImpl.java
@@ -0,0 +1,17 @@
+public class SampleableListImpl extends ArrayList implements SampleableList {
+	
+	/**
+	 *	{@see SampleableList} for description of the method below
+	 */
+	
+	public SampleableList sample() {
+		SampleableList sample = new SampleableListImpl();
+		int length = size();
+		for (int i = 0; i < length; i = i + 2) {
+			Object objectToAdd = get(i).getReturnValue();
+			sample.add(objectToAdd);
+		}
+		return sample;
+	}
+	
+}
